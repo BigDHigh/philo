8e86c0860c5e2b13a42ac201e04fb44efd595bf8
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-11-22 21:28:44 +0100

    fixed: used wrong mutex

--- philo/one_philo.c
+++ philo/one_philo.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 20:27:59 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/11/22 20:45:10 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/11/22 21:28:28 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*just_die(t_data *data)
 {
-	pthread_mutex_lock(&data->run_mutex);
+	pthread_mutex_lock(&data->print_mutex);
 	printf("0 1 has taken a fork\n");
 	usleep(data->time_to_die * 1000);
 	printf("%i 1 died\n", data->time_to_die);
-	pthread_mutex_unlock(&data->run_mutex);
+	pthread_mutex_unlock(&data->print_mutex);
 	return (NULL);
 }
 
